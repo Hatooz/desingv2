3fc558efd3e61c4746a1aa006ec1c7d852815666
Hatem Mohi El Din <hatem.mohieldin@gmail.com>
2019-11-29 13:51:19 +0000

    kmom03

--- config/page.php
+++ config/page.php
@@ -21,7 +21,7 @@ return [
             "stylesheets" => [
                 // Change here to set another default stylesheet
                 // "css/dbwebb-se.min.css",
-                "css/kmom02.css",
+                "css/kmom03.css",
             ],
             "javascripts" => [
                 "js/responsive-menu.js",
